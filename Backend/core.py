93ae787cb1ffea816e50504e1ecfecd1f59fb716
Medwedkas <79253741+Medwedkas@users.noreply.github.com>
2021-12-21 23:59:33 +0300

    Delete Backend directory

--- Backend/core.py
+++
@@ -1,6 +0,0 @@
-import json
-
-config = json.load(open('config.json', 'r'))
-web_config = config['web']
-db_config = config['db']
-token_config = config['token']
--- Backend/source/api/rest/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_jsonschema_validator import JSONSchemaValidator
-
-app = Flask(__name__)
-JSONSchemaValidator(app=app, root="source/api/rest/schemas")
-
-from .rest import *
-from .error_handlers import *
