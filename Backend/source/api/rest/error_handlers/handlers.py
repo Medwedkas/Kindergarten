fb381b99dec7d975d5edee6ab4e1d3def31d49f3
Medwedkas <79253741+Medwedkas@users.noreply.github.com>
2021-12-21 23:52:40 +0300

    Delete Backend/source/api/rest directory

--- Backend/source/api/rest/error_handlers/handlers.py
+++
@@ -1,8 +0,0 @@
-from .. import app
-from jsonschema import ValidationError
-from ..utils import Reply
-
-
-@app.errorhandler(ValidationError)
-def on_validation_error(e):
-    return Reply.bad_request(error=str(e).replace('\n', ' ').strip())
